0103b5b35e713bcf6a24bca27b2071735e62a997
secretlycharles <imcharlesmendez@gmail.com>
2026-04-03 15:54:06 -0700

    learned how to print and comment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    // This is a comment inside of rust
     println!("Hello, world!");
+
+    /*
+        This is a multi-lined comment inside of rust
+     */
+    println!("I'm a Rustacean!")
 }
